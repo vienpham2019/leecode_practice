bb67b4bfe2e4ddd57dcff4592556510677c6a31a
vienpham2019 <vienpham2019@gmail.com>
2020-07-09 16:34:32 -0500

    save

--- remove_nth_node_from_end_of_list.js
+++ remove_nth_node_from_end_of_list.js
@@ -9,9 +9,23 @@ class ListNode {
     }
 }
 
-// var removeNthFromEnd = function(head, n) {
-    
-// };
+var removeNthFromEnd = function(head, n) {
+    let dummy_lists = new ListNode()
+    dummy_lists.next = head 
+    let slow = dummy_lists 
+    let fast = dummy_lists 
+    for(let i = 1; i <= n + 1 ; i ++){
+        fast = fast.next 
+    }
+
+    while(fast !== null){
+        fast = fast.next 
+        slow = slow.next
+    }
+
+    slow.next = slow.next.next
+    return dummy_lists.next
+};
 
 let lists = new ListNode(1)
 lists.next = new ListNode(2)
@@ -19,4 +33,4 @@ lists.next.next = new ListNode(3)
 lists.next.next.next = new ListNode(4)
 lists.next.next.next.next = new ListNode(5)
 
-console.log(lists)
+console.log(removeNthFromEnd(lists, 2))
