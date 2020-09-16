9a10ed9884f94f1ec79c357feacb243cb6cf33bb
vienpham2019 <vienpham2019@gmail.com>
2020-09-15 22:03:12 -0500

    add multiply string

--- medium/Multiply_Strings.js
+++ medium/Multiply_Strings.js
@@ -16,53 +16,20 @@
 
 var multiply = function(num1, num2) {
     if(num1 === '0' || num2 === '0') return '0'
-    let result = '0'
+    let result = new Array(num1.length + num2.length).fill(0)
 
-    let n_1 , n_2
-
-    if(num1.length > num2.length) {
-        n_1 = num1
-        n_2 = num2
-    }else{
-        n_1 = num2
-        n_2 = num1
-    }
-
-    for(let i = n_2.length - 1; i >= 0 ; i --){
-        let remain = 0 
-        let sum = `${remain}`
-        for(let j = n_1.length - 1 ; j >= 0 ; j --){
-            let total = (n_2[i] - '0') * (n_1[j] - '0')
-            let zero = `${(10 ** (n_1.length - 1 - j))}`
-            let t = `${total}` + zero.slice(1,zero.length)
-            remain = Math.floor(total / 10)
-            sum = add_string(sum , t)
+    for(let i = num2.length - 1; i >= 0 ; i --){
+        for(let j = num1.length - 1 ; j >= 0 ; j --){
+            let total = (num2[i] - '0') * (num1[j] - '0')
+            let sum = result[i + j + 1] + total
+            result[i + j + 1] = sum % 10 
+            result[i + j] += Math.floor(total / 10)
         }
-        let zero_2 = `${10 ** (n_2.length - 1 - i)}`
-        sum += zero_2.slice(1,zero_2.length)
-        result = add_string(result , sum)
     }
 
-    return result 
+    if(result[0] === 0) result.shift()
+
+    return result.join('')
 }
 
-const add_string = (num1 , num2) => {
-    let i = num1.length - 1
-    let j = num2.length - 1 
-    let carry = 0 
-    let result = ''
-    while(i >= 0 || j >= 0){
-        let sum_1 = carry 
-        if(i >= 0) sum_1 += num1.charAt(i--) - '0'
-        if(j >= 0) sum_1 += num2.charAt(j--) - '0'
-
-        result = ( sum_1 % 10 ) + result
-        carry = Math.floor(sum_1 / 10)
-    }
-    if(carry !== 0) result = carry + result
-
-    return result
-}
-
-console.log(multiply("401716832807512840963",
-"167141802233061013023557397451289113296441069"))
+console.log(multiply("9" , "9"))
