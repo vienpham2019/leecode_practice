aca3610ab2e32dbf4a15689bdd8eb734778ad7a0
vienpham2019 <vienpham2019@gmail.com>
2020-10-07 22:01:29 -0500

    add diameter of binary tree

---
+++ easy/Diameter_of_Binary_Tree.js
@@ -0,0 +1,25 @@
+// Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+// Example:
+// Given a binary tree
+//           1
+//          / \
+//         2   3
+//        / \     
+//       4   5    
+// Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+// Note: The length of path between two nodes is represented by the number of edges between them.
+
+const diameterOfBinaryTree = root => {
+    this.answer = 1 
+    const depth = node => {
+        if(node === null) return 0 
+        let left = depth(node.left)
+        let right = depth(node.right)
+        this.answer = Math.max(this.answer , left + right + 1)
+        return Math.max(left , right ) + 1
+    }
+    depth(root)
+    return this.answer - 1
+}
